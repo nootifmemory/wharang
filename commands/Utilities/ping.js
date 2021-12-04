02919538dd057a9ad78989b1f1a6f91081718342
nootifmemory <nootifhuhong@gmail.com>
2021-12-04 17:41:30 +0700

    update again

--- commands/Utilities/ping.js
+++ commands/Utilities/ping.js
@@ -3,6 +3,6 @@ module.exports = {
   description : 'this is the ping command , for checking latency',
   cooldown : 3 ,
   run(client, message, args, Discord) {
-    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
+    message.channel.send(`Message latency is ${Date.now() - message.createdTimestamp}ms. WS latency is ${Math.round(client.ws.ping)}ms`)
   }
 }
